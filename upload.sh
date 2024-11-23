98f754412d02670f2a8230e6474c8bbc01bfaeae
Juan Bindez <juanbindez780@gmail.com>
2024-11-22 22:22:47 -0300

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VERSION=1
+MINOR=0
+PATCH=0
+EXTRAVERSION=""
+
+NOTES="(update)"
+BRANCH="main"
+
+if [[ -z $PATCH ]]; then
+    PATCH=""
+else
+    PATCH=".$PATCH"
+fi
+
+if [[ $EXTRAVERSION == *"-rc"* ]]; then
+    FULL_VERSION="$VERSION.$MINOR$PATCH$EXTRAVERSION"
+else
+
+    if [[ -z $EXTRAVERSION ]]; then
+        FULL_VERSION="$VERSION.$MINOR$PATCH"
+    else
+        FULL_VERSION="$VERSION.$MINOR$PATCH.$EXTRAVERSION"
+    fi
+fi
+
+git add .
+git commit -m "$FULL_VERSION $NOTES"
+git push -u origin $BRANCH
+git tag v$FULL_VERSION
+git push --tags
